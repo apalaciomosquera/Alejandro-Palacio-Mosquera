cfc43147ab421bc6945d25144a444a09a3968415
apalaciomosquera <144925560+apalaciomosquera@users.noreply.github.com>
2024-08-28 18:48:13 -0500

    config.py

---
+++ Parcial1/microUsers/config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'root'
+    MYSQL_DB = 'myflaskapp'
+    SQLALCHEMY_DATABASE_URI = f'mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}'
